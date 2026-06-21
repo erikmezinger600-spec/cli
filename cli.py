2cd72b6717ff0efebbb57d8aeaa182f336fba150
erikmezinger600-spec <erikmezinger600@gmail.com>
2026-06-21 18:40:00 +0300

    Implement basic CLI with command handling

---
+++ cli.py
@@ -0,0 +1,30 @@
+import sys
+
+from commands.help import help_cmd
+from commands.ls import ls_cmd
+from commands.rem import rem_cmd
+from commands.mkdir import mkdir_cmd
+from commands.pwd import pwd_cmd
+from commands.version import version_cmd
+
+COMMANDS = {
+    "help": help_cmd,
+    "ls": ls_cmd,
+    "rem": rem_cmd,
+    "mkdir": mkdir_cmd,
+    "pwd": pwd_cmd,
+    "version": version_cmd
+}
+
+if len(sys.argv) < 2:
+    print("CLI v1.0")
+    print("Type: cli help")
+    exit()
+
+cmd = sys.argv[1]
+args = sys.argv[2:]
+
+if cmd in COMMANDS:
+    COMMANDS[cmd](args)
+else:
+    print(f"Unknown command: {cmd}")
