6ace197ae8019079ae197e3bc627272a47f4c47a
erikmezinger600-spec <erikmezinger600@gmail.com>
2026-06-21 18:40:45 +0300

    Add help command with CLI usage instructions

---
+++ commands/help.py
@@ -0,0 +1,11 @@
+def help_cmd(args):
+    print("""
+CLI Commands
+
+help        Show help
+ls          List files
+pwd         Show current directory
+mkdir       Create directory
+rem         Delete file/folder
+version     Show version
+""")
